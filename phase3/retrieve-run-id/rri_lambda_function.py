ab0d2f5786d75ef4ed08688ce22d94da67118938
eriiire <eri.eriagbaraoluwa.nhs.scot>
2023-07-31 13:42:09 +0000

    create utils.py file and import it into respective lambda_functions

--- phase3/retrieve-run-id/rri_lambda_function.py
+++ phase3/retrieve-run-id/rri_lambda_function.py
@@ -4,38 +4,7 @@ import ast
 import pymysql
 import json
 
-def get_secret():
-
-    secret_name = "prod/LambdaStepCompute/phase3"
-    region_name = "us-east-1"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
-
-    # Decrypts secret using the associated KMS key.
-    secret = get_secret_value_response['SecretString']
-
-    try:
-        # convert string type to dictionary
-        dict_secret = ast.literal_eval(secret)
-
-    except SyntaxError:
-        dict_secret = {}
-
-    return dict_secret
+from utils import get_secret
 
 
 def get_max_run_id():
@@ -68,6 +37,7 @@ def get_max_run_id():
     return run_id
 
 def handler(event, context):
+
     # Extract the number of repetitions from the event
     repetitions = event['repetitions']
     
